2a4d02f18aabe63d1cd89e4a2bd1096f7f095ab6
Fatimah <fatimah@Fatimahs-MBP.broadband>
2019-11-22 12:14:47 +0000

    added prevent default to form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Image from "./Images/Banner.jpg";
 import Header from "./Components/Header";
-import Form from "./Components/Form";
+// import Form from "./Components/Form";
 
 class App extends React.Component {
   render() {
@@ -15,7 +15,19 @@ class App extends React.Component {
 
         <div className="App">
           <Header />
-          <Form />
+          {/* <Form /> */}
+          <div>
+            <p>Search an Artist to learn more about their lyrics below</p>
+            <form onSubmit={e => e.preventDefault()}>
+              <input
+                className="artistinput"
+                type="text"
+                placeholder="Enter the Artist's name here..."
+              />
+              <br />
+              <button type="submit">Get Results</button>
+            </form>
+          </div>
         </div>
       </div>
     );
