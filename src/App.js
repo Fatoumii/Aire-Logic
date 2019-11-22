0fbe67653c487218dbe3c411039988d590147be8
Fatimah <fatimah@Fatimahs-MBP.broadband>
2019-11-22 11:27:00 +0000

    initialised react and added basic banner and heading

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Image from "./Images/Banner.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <img
+          src={Image}
+          alt="woman standing in front of a instrumental neon backdrop"
+        />
+
+        <div className="App">
+          <h1>Artist Data</h1>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
