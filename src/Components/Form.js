ed3f097b2e55c9e46ca396a47f3ea8b40c64cbcd
Fatimah <fatimah@Fatimahs-MBP.broadband>
2019-11-24 01:16:33 +0000

    removed 1s and rounded average

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -25,34 +25,50 @@ class Form extends React.Component {
           />
           <br />
           <button type="submit">Get Results</button>
+          <button onClick={this.handleClick}>Reset</button>
         </form>
-        <p>
-          {lengthOfSongs.length > 1
-            ? `The average words ${inputArtist[0].toUpperCase() +
-                inputArtist.slice(1)} has in her songs are ${this.getAverage(
-                lengthOfSongs
-              )}!`
-            : null}
-        </p>
-        <p>
-          {lengthOfSongs.length > 1
-            ? `${inputArtist[0].toUpperCase() +
-                inputArtist.slice(1)}'s shortest song has ${
-                lengthOfSongs.sort((a, b) => {
-                  return a - b;
-                })[0]
-              } words, while the longest song has ${
-                lengthOfSongs.sort((a, b) => {
-                  return a - b;
-                })[lengthOfSongs.length - 1]
-              } words. Cool hey..`
-            : null}
-        </p>
+        <div className="result">
+          <p>
+            {lengthOfSongs.length > 1
+              ? `The average words ${inputArtist[0].toUpperCase() +
+                  inputArtist.slice(1)} has in her songs are ${Math.round(
+                  this.getAverage(lengthOfSongs)
+                )}!`
+              : null}
+          </p>
+          <p>
+            {lengthOfSongs.length > 1
+              ? `${inputArtist[0].toUpperCase() +
+                  inputArtist.slice(1)}'s shortest song has ${
+                  lengthOfSongs.sort((a, b) => {
+                    return a - b;
+                  })[0]
+                } words, while the longest song has ${
+                  lengthOfSongs.sort((a, b) => {
+                    return a - b;
+                  })[lengthOfSongs.length - 1]
+                } words.`
+              : null}
+          </p>
+        </div>
       </div>
     );
   }
-
+  handleClick = () => {
+    this.setState({
+      inputArtist: "",
+      artistID: "",
+      listofTracks: [],
+      lengthOfSongs: []
+    });
+  };
   getAverage = lengthOfSongs => {
+    for (let i = 0; i < lengthOfSongs.length; i++) {
+      if (lengthOfSongs[i] === 1) {
+        lengthOfSongs.splice(i, 1);
+      }
+    }
+
     let sum = 0;
     for (let i = 0; i < lengthOfSongs.length; i++) {
       sum += lengthOfSongs[i];
